2ea566642c0f6aa830eb95e4bd6fdd44c59c4ca8
Jakson Kallio <jakson@protonmail.com>
2018-12-12 11:47:38 -0600

    DisplayArray

--- src/jaksonkallio/sort/Main.java
+++ src/jaksonkallio/sort/Main.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.System;
+import java.lang.StringBuilder;
 
 class Main {
 	public static void main(String[] args){
@@ -22,15 +23,32 @@ class Main {
 
 		for(int i = 0; i < algos.size(); i++){
 			SortingAlgorithm algo = algos.get(i);
+			int[] sorted = new int[SAMPLE_ELEMENT_COUNT];
 
 			long bench_start = System.currentTimeMillis();
-			algo.sort(sample_input);
+			sorted = algo.sort(sample_input);
+			displayArray(sorted);
 			long bench_end = System.currentTimeMillis();
 
 			System.out.println(algo.getName() + " performed sort in " + (bench_end - bench_start) + "ms");
 		}
 
 		System.out.println("Tests complete");
+	}
+
+	private void displayArray(int[] arr){
+		StringBuilder str = new StringBuilder();
+		str.append("[");
+		for(int i = 0; i < arr.length; i++){
+			if(i != 0){
+				str.append(",");
+			}
+
+			str.append(arr[i]);
+		}
+		str.append("]");
+
+		System.out.println(str);
 	}
 
 	private List<SortingAlgorithm> getAlgorithms(){
@@ -44,14 +62,14 @@ class Main {
 	private int[] genSampleInput(int elements){
 		int[] sample_input = new int[elements];
 		for(int i = 0; i < sample_input.length; i++){
-			sample_input[i] = sample_input_gen.nextInt();
+			sample_input[i] = Math.abs(sample_input_gen.nextInt());
 		}
 
 		return sample_input;
 	}
 
-	public static final long SAMPLE_INPUT_SEED = 101;
-	public static final int SAMPLE_ELEMENT_COUNT = 1000;
+	public static final long SAMPLE_INPUT_SEED = 100;
+	public static final int SAMPLE_ELEMENT_COUNT = 10;
 
 	private final Random sample_input_gen;
 }
