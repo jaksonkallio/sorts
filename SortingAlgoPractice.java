97344617f666f64b7da5579f2a269e295e5d0454
Jakson Kallio <jakson@protonmail.com>
2018-12-11 19:33:21 -0600

    Sorting algo practice base

---
+++ SortingAlgoPractice.java
@@ -0,0 +1,11 @@
+class SortingAlgoPractice {
+	public static void main(String[] args){
+		(new SortingAlgoPractice()).runTests();
+	}
+
+	public void runTests(){
+		System.out.println("Running tests...");
+
+		System.out.println("Tests complete");
+	}
+}
